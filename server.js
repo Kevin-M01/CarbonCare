78396dae21e548a3598bb0ba6a83e35e51f7b8a6
Arnesh Jairath <arnesh.jairath@hotmail.com>
2021-02-14 12:08:08 -0500

    work

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const app = express();
 app.use(express.static(__dirname + '/dist/co2-calculator-rev2'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname "dist", "co2-calculator-rev2", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "co2-calculator-rev2", "index.html"));
 });
+
+app.get("/test", function (req, res){
+    return res.send("Hello World")
+})
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000);

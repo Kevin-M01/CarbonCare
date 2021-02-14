8c2601ede894738c246fc31d57d9f3aeca2d102c
Eugene Koo <eugenekoo777@gmail.com>
2021-02-13 20:18:53 -0500

    stuff

--- carbon-calculator/src/App.js
+++ carbon-calculator/src/App.js
@@ -1,34 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-import TextField from '@material-ui/core/TextField';
+import React from 'react';
+import { 
+  makeStyles, FormLabel, FormControl, FormGroup, FormControlLabel, FormHelperText, Checkbox,
+  TextField,
+  Grid, Typography 
+  } from '@material-ui/core';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
- 
-    <div className="InputSection">
-      <label class="mdc-text-field mdc-text-field--filled">
-        <span class="mdc-text-field__ripple"></span>
-        <span class="mdc-floating-label" id="my-label-id">Hint text</span>
-        <input class="mdc-text-field__input" type="text" aria-labelledby="my-label-id"></input>
-        <span class="mdc-line-ripple"></span>
-      </label>
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+     
+
+
+<React.Fragment>      
+      <Typography variant="h6" gutterBottom>
+        Send Help Please    
+      </Typography>
+      
+
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            
+            // onChange = {this.sayHello}
+            // value= {this.state.textFieldValue}            
+
+            required
+            id="firstName"
+            name="firstName"
+            label="First name"
+            fullWidth
+            autoComplete="given-name"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="lastName"
+            name="lastName"
+            label="Last name"
+            fullWidth
+            autoComplete="family-name"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="address1"
+            name="address1"
+            label="Address line 1"
+            fullWidth
+            autoComplete="shipping address-line1"
+          />
+        </Grid>
+        <Grid item xs={12} sm = {6}>
+          <TextField
+            id="address2"
+            name="address2"
+            label="Address line 2"
+            fullWidth
+            autoComplete="shipping address-line2"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+            label="Use this address for payment details"
+          />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+    
     </div>
+    
   );
 }
 

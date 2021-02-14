257bd69c4c87289f9fbde228c2d79db81ccb2706
Arnesh Jairath <arnesh.jairath@hotmail.com>
2021-02-14 11:55:00 -0500

    temp

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/co2-calculator-rev2'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/co2-calculator-rev2/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/co2-calculator-rev2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
